256f98923ed3b1dbd5e79655325bb2edca72ccc7
wanghm <568019240@qq.com>
2023-08-31 20:31:23 +0800

    add gpytorch

--- algorithms/GP_gpytorch.py
+++ algorithms/GP_gpytorch.py
@@ -1,85 +1,69 @@
-sssss
 import torch
 import gpytorch
+from gpytorch.means import ConstantMean
+from gpytorch.kernels import ScaleKernel, RBFKernel
+from gpytorch.distributions import MultivariateNormal
 
-class GPStateTransitionModel:
-    class CustomGP(gpytorch.models.ExactGP):
-        def __init__(self, train_x, train_y, likelihood):
-            super().__init__(train_x, train_y, likelihood)
-            self.mean_module = gpytorch.means.ConstantMean()
-            self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
+# 1. 定义数据集
+num_samples = 100
+state_dim = 96
+action_space = 6
 
-        def forward(self, x):
-            mean_x = self.mean_module(x)
-            covar_x = self.covar_module(x)
-            return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+# 随机生成状态和动作数据
+states = torch.randn(num_samples, state_dim)
+actions = torch.randint(0, action_space, (num_samples,))
 
-    def __init__(self, action_dim=6, state_dim=96):
-        self.model_s_prime = None
-        self.model_r = None
-        self.likelihood_s_prime = gpytorch.likelihoods.GaussianLikelihood()
-        self.likelihood_r = gpytorch.likelihoods.GaussianLikelihood()
-        self.action_dim = action_dim
-        self.state_dim = state_dim
+# 将动作转换为独热编码
+actions_onehot = torch.nn.functional.one_hot(actions, action_space).float()
+inputs = torch.cat([states, actions_onehot], dim=-1)
 
-    def train(self, states, actions, s_prime, rewards, epochs=100, learning_rate=0.1):
-        actions_one_hot = torch.eye(self.action_dim)[actions]
-        s_a = torch.cat([states, actions_one_hot], dim=1)
+# 这里只是模拟生成s'和r，你应该使用你的真实数据
+next_states = states + 0.1 * torch.randn(num_samples, state_dim)
+rewards = torch.randn(num_samples, 1)
+outputs = torch.cat([next_states, rewards], dim=-1)
 
-        self.model_s_prime = self.CustomGP(s_a, s_prime, self.likelihood_s_prime)
-        self.model_r = self.CustomGP(s_a, rewards, self.likelihood_r)
+# 2. 定义高斯过程模型
+class GPModel(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood):
+        super(GPModel, self).__init__(train_x, train_y, likelihood)
+        self.mean_module = ConstantMean()
+        self.covar_module = ScaleKernel(RBFKernel(ard_num_dims=train_x.size(-1)))
 
-        optimizer = torch.optim.Adam([
-            {'params': self.model_s_prime.parameters()},
-            {'params': self.model_r.parameters()}
-        ], lr=learning_rate)
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return MultivariateNormal(mean_x, covar_x)
 
-        mll_s_prime = gpytorch.mlls.ExactMarginalLogLikelihood(self.likelihood_s_prime, self.model_s_prime)
-        mll_r = gpytorch.mlls.ExactMarginalLogLikelihood(self.likelihood_r, self.model_r)
+likelihood = gpytorch.likelihoods.GaussianLikelihood()
+model = GPModel(inputs, outputs, likelihood)
 
-        self.model_s_prime.train()
-        self.model_r.train()
-        self.likelihood_s_prime.train()
-        self.likelihood_r.train()
+# 3. 定义一个训练循环
+def train(model, likelihood, inputs, outputs, epochs=100):
+    model.train()
+    likelihood.train()
 
-        for epoch in range(epochs):
-            optimizer.zero_grad()
-            output_s_prime = self.model_s_prime(s_a)
-            output_r = self.model_r(s_a)
-            pred_s_prime = self.likelihood_s_prime(output_s_prime)
-            pred_r = self.likelihood_r(output_r)
-            loss = -mll_s_prime(pred_s_prime, s_prime) - mll_r(pred_r, rewards)
-            loss.backward()
-            optimizer.step()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+    mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
 
-    def predict(self, states, actions):
-        actions_one_hot = torch.eye(self.action_dim)[actions]
-        s_a = torch.cat([states, actions_one_hot], dim=1)
+    for i in range(epochs):
+        optimizer.zero_grad()
+        output = model(inputs)
+        loss = -mll(output, outputs)
+        loss.backward()
+        optimizer.step()
 
-        self.model_s_prime.eval()
-        self.model_r.eval()
-        self.likelihood_s_prime.eval()
-        self.likelihood_r.eval()
+# 4. 训练模型
+train(model, likelihood, inputs, outputs)
 
-        with torch.no_grad(), gpytorch.settings.fast_pred_var():
-            s_prime_pred = self.likelihood_s_prime(self.model_s_prime(s_a))
-            r_pred = self.likelihood_r(self.model_r(s_a))
+# 5. 使用模型进行预测
+model.eval()
+likelihood.eval()
+with torch.no_grad(), gpytorch.settings.fast_pred_var():
+    test_states = torch.randn(2, state_dim)
+    test_actions = torch.tensor([1, 4]) # 只是示例动作
+    test_actions_onehot = torch.nn.functional.one_hot(test_actions, action_space).float()
+    test_inputs = torch.cat([test_states, test_actions_onehot], dim=-1)
+    observed_pred = likelihood(model(test_inputs))
 
-        return s_prime_pred.mean, r_pred.mean
-
-# 示例
-if __name__ == "__main__":
-    states_train = torch.rand(100, 96)
-    actions_train = torch.randint(0, 6, (100,))
-    s_prime_train = torch.rand(100, 96)
-    rewards_train = torch.rand(100, 1)
-
-    model = GPStateTransitionModel()
-    model.train(states_train, actions_train, s_prime_train, rewards_train)
-
-    states_test = torch.rand(5, 96)
-    actions_test = torch.randint(0, 6, (5,))
-    s_prime_pred, r_pred = model.predict(states_test, actions_test)
-    print("Predicted s':", s_prime_pred)
-    print("Predicted r:", r_pred)
+print(observed_pred.mean)
 
